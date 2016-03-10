0d5f3b0b091b439316777ff4047c8209b9080252
Olivia Choy <olivia.choy2@gmail.com>
2016-03-10 15:09:30 -0500

    more styling changes

--- front-page.php
+++ front-page.php
@@ -25,39 +25,62 @@
 
 		<section class="skills">
 			<h2><?php the_field('skills_title');?></h2>
-			
-			<div class="skillIcons">
-				<?php if( have_rows('skill-icons')): ?>
-				<?php while( have_rows('skill-icons')): the_row(); ?>
-				<i class="fa <?php the_sub_field('icon')?>"></i>
-				<p class="caption">
-					<?php get_sub_field('caption')?>
-				</p>
-				<?php endwhile; ?>
-			<?php endif; ?>
-			</div>
-			<p>
+			<div class="skillsContainer">
+				<div class="skillIcons">
+					<?php if( have_rows('skill-icons')): ?>
+					<?php while( have_rows('skill-icons')): the_row(); ?>
+					<div class="icons">
+						<i class="<?php the_sub_field('icon')?>"></i>
+						<p class="caption">
+							<?php the_sub_field('caption')?>
+						</p>
+					</div>
+					<?php endwhile; ?>
+				<?php endif; ?>
+				</div>
 				<?php the_field('service_description');?>
-			</p>
+			</div>
 		</section>
 		
 		<section class="portfolioSection">
-			<?php $portfolioQuery = new WP_Query(array(
-			  'posts_per_page' => 2,
-			  'post_type' => 'portfolio' 
-			)); ?>
+			<div class="portfolioContent">
+				<?php $portfolioQuery = new WP_Query(array(
+				  'posts_per_page' => 2,
+				  'post_type' => 'portfolio' 
+				)); ?>
 
-			<?php if($portfolioQuery-> have_posts()): ?>
-			  <?php while($portfolioQuery-> have_posts()): ?>
-			    <?php $portfolioQuery-> the_post(); ?>
-			    <!-- what we want to show goes here -->
-			    <h3><?php the_title(); ?></h3>
-			 <?php while(has_sub_field('skill-icon')): ?>
-			  
-			    <?php endwhile; ?>
-			  <?php endwhile; ?>
-			  <?php wp_reset_postdata(); ?>
-			<?php  endif; ?>
+				<?php if($portfolioQuery-> have_posts()): ?>
+				  <?php while($portfolioQuery-> have_posts()): ?>
+				    <?php $portfolioQuery-> the_post(); ?>
+				    <!-- what we want to show goes here -->
+				    <div class="portfolioDescription">
+					 	<img src="<?php echo hackeryou_get_thumbnail_url($post) ?>" alt="">
+					 	<div class="portfolioInfo">
+					    	<h3><?php the_title(); ?></h3>
+						 	<?php the_content();?>
+					 	</div>
+				 	</div>
+				  <?php endwhile; ?>
+				  <?php wp_reset_postdata(); ?>
+				<?php  endif; ?>
+				<div id="right-triangle"></div>
+			</div>
+		</section>
+
+		<section class="contact">
+			<h2><?php the_field('contact_title') ?></h2>
+			<div class="contactContainer">
+				<div class="contactIcons">
+				<?php if( have_rows('contact_icons')): ?>
+					<?php while( have_rows('contact_icons')): the_row(); ?>
+						<?php the_sub_field('icon') ?>
+						<p class="caption">
+							<?php the_sub_field('caption')?>
+						</p>
+					<?php endwhile; ?>
+				<?php endif; ?>
+				</div>
+			</div>
 		</section>
 
       <?php endwhile; // end the loop?>
