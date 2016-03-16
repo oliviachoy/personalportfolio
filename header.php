fbdf1489973d082c7e0d3f05c127e5f14ecf4ca5
Olivia Choy <olivia.choy2@gmail.com>
2016-03-16 16:39:19 -0400

    made changes

--- header.php
+++ header.php
@@ -6,7 +6,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php  wp_title('|', true, 'right'); ?></title>
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png" />
   <!-- stylesheets should be enqueued in functions.php -->
   <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/simpletextrotator.css" media="screen"/>
   <?php wp_head(); ?>
